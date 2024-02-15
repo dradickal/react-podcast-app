2744fb9a5de6a836c0885487ece5faee16b321b0
David Radick <dradick@radickalcreations.com>
2024-02-15 15:50:08 -0600

    animating hide/show of series description

--- src/SeriesDescription.jsx
+++ src/SeriesDescription.jsx
@@ -1,11 +1,12 @@
-import captureKeyboardEvent from "./util/captureKeyboardEvent";
+import captureKeyboardEvent from './util/captureKeyboardEvent';
+import './SeriesDescription.css';
 
 export function SeriesDescription() {
     function toggleDescription(e) {
         e.preventDefault();
         const el = e.currentTarget;
         const descID = el.attributes.getNamedItem('aria-controls').value;
-        const descExpanded = el.ariaExpanded === "true";
+        const descExpanded = el.ariaExpanded === 'true';
         const desc = document.getElementById(descID);
         const hide = () => { desc.classList.add('hide'); desc.ariaHidden = true; }
         const show = () => { desc.classList.remove('hide'); desc.ariaHidden = false; }
@@ -15,7 +16,7 @@ export function SeriesDescription() {
     }
     
     function keyboardTrigger(e) {
-        if(captureKeyboardEvent(e.nativeEvent, "Enter", " ")) {
+        if(captureKeyboardEvent(e.nativeEvent, 'Enter', ' ')) {
             toggleDescription(e);
         }
 
@@ -26,17 +27,18 @@ export function SeriesDescription() {
         <>
             <section className="header-series">
                 <h1>The Most Interesting People</h1>
-                <span className="series-description-button" 
+                <span 
+                    className="series-description-button" 
                     role="button" 
                     onClick={toggleDescription}
                     onKeyDown={keyboardTrigger} 
                     tabIndex="0" 
-                    aria-expanded="true" 
+                    aria-expanded="false" 
                     aria-controls="series-description"
-                    aria-label="Toggle Series Description">
+                    aria-label="Toggle view of series description">
                     <i className="fas fa-info-circle"></i>
                 </span>
-                <div id="series-description" className="series-description">
+                <div id="series-description" className="series-description hide" aria-hidden="true">
                     <p>Listen to the most interesting people from around the world share thier stories in this podcast from RKC.</p>
                     <p className="credits"><b className="role">Host:</b> Jonathan Goldsmith</p>
                     <p className="credits"><b className="role">Produced by:</b> RKC</p>
