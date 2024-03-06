b55c4596ec59d7af62a4dcd778834a47c1f6718f
David Radick <dradick@radickalcreations.com>
2024-03-05 18:07:33 -0600

    create series data structure

---
+++ server/seriesData.js
@@ -0,0 +1,80 @@
+const seriesData = {
+    '99-percent-invisible': {
+        'apiId': '938de7f853b24bf59c7d199b8fba3950',
+        'title': '99% Invisible',
+        'image': 'https://production.listennotes.com/podcasts/99-invisible-roman-mars-vQyoZCSNjaI-JqxekyhmQi1.300x300.jpg',
+        'description': '<p>Design is everywhere in our lives, perhaps most importantly in the places where we\'ve just stopped noticing. 99% Invisible is a weekly exploration of the process and power of design and architecture. From award winning producer Roman Mars. Learn more at 99percentinvisible.org.</p>',
+        'credits': {
+            'host': 'Roman Mars',
+            'publisher': '99% Invisible',
+            'network': 'Stitcher',
+        },
+    },
+    'dev-interrupted': {
+        'apiId': '0e23d776718b48d0a7e2d6f17e977cc3',
+        'title': 'Dev Interrupted',
+        'image': 'https://production.listennotes.com/podcasts/dev-interrupted-linearb-MZqbsRpe5Fn-N9IMxKrTc4P.300x300.jpg',
+        'description': '<p>The Dev Interrupted Podcast is the premier podcast made exclusively for software engineering leaders. Hosts Dan Lines &amp; Conor Bronsdon invite expert guests from around the world to explore strategy and day-to-day topics ranging from dev team metrics to accelerating delivery. Join us weekly for new episodes.</p>',
+        'credits': {
+            'host': 'Conor Bronsdon',
+            'publisher': 'LinearB',
+        },
+    },
+    'ear-hustle': {
+        'apiId': '25bb8be5c78d4b7cbd2acc1a29b39ffb',
+        'title': 'Ear Hustle',
+        'image': 'https://production.listennotes.com/podcasts/ear-hustle-ear-hustle-radiotopia-wxAP0JqQVZh-UIa8f_H0hut.300x300.jpg',
+        'description':'<p>Ear Hustle is prison slang for eavesdropping, and that’s what listening to the show feels like: a raw, often funny, and always surprising peek into the reality of life inside prison. </p><p>Hosts Nigel Poor and Earlonne Woods co-created the show that launched in 2017 while Earlonne was incarcerated at San Quentin State Prison, where Nigel was a volunteer teaching photography. Since Earlonne’s release in 2018, the show has expanded to include stories from prisons across the state, including the California Institution for Women, as well as stories about getting out of prison and starting over, post-incarceration.</p><p>From finding romance, to grappling with a life sentence, to trying to parent via 15-minute phone calls, Ear Hustle stories deliver what This American Life host Ira Glass calls a “very real” and “untragic” take on prison life. </p><p>Ear Hustle is a proud member of Radiotopia from PRX, a network of independent listener-supported podcasts. Discover audio with vision at radiotopia.fm and learn more about Ear Hustle at earhustlesq.com.</p>',
+        'credits': {
+            'host': ['Nigel Poor', 'Earlonne Woods'],
+            'publisher': 'Ear Hustle',
+            'network': 'Radiotopia by PRX',
+        },
+    },
+    'song-exploder': {
+        'apiId': '8d8970470ead450aa5a9d2a1d223d938',
+        'title': 'Song Exploder',
+        'image': 'https://production.listennotes.com/podcasts/song-exploder-hrishikesh-hirway-1FNLDiuvXbO-Ut3fuw2xdYv.300x300.jpg',
+        'description': '<p>Song Exploder is a podcast where musicians take apart their songs, and piece by piece, tell the story of how they were made. Each episode features an artist discussing a song of theirs, breaking down the sounds and ideas that went into the writing and recording. Hosted and produced by Hrishikesh Hirway.</p>',
+        'credits': {
+            'host': 'Hrishikesh Hirway',
+            'publisher': 'Hrishikesh Hirway',
+            'network': 'Radiotopia by PRX',
+        },
+    },
+    'switched-on-pop': {
+        'apiId': '36ad4ab8d4934013911a5370b84ecfa3',
+        'title': 'Switched on Pop',
+        'image': 'https://production.listennotes.com/podcasts/switched-on-pop-vulture-AeJREDWYCOa-YAyMJOkLo-7.300x300.jpg',
+        'description': '<p>A podcast all about the making and meaning of popular music. Musicologist Nate Sloan & songwriter Charlie Harding pull back the curtain on how pop hits work magic on our ears & our culture. From Vulture and the Vox Media Podcast Network.</p>',
+        'credits': {
+            'host': ['Nate Sloan', 'Charlie Harding'],
+            'publisher': 'Vulture',
+            'network': 'Vox Media Podcast Network',
+        },
+    },
+    'the-dan-patrick-show': {
+        'apiId': '213cee4a8eda4306a8a809f46d78616a',
+        'title': 'The Dan Patrick Show',
+        'image': 'https://production.listennotes.com/podcasts/the-dan-patrick-show-iheartpodcasts-and-dan--tGaiY7kCZ8-fN2SWqxK4FJ.300x300.jpg',
+        'description': '<p>Listen to Dan\'s daily radio show. With exclusive insider access, Patrick brings A-list guests from the world of sports and entertainment to the show. Sharing his perspective on pop culture and sports, Patrick also brings a dose of humor to his fans.</p>',
+        'credits': {
+            'host': 'Dan Patrick',
+            'publisher': 'Dan Patrick Productions',
+            'network': 'iHeartPodcasts',
+        },
+    },
+    'this-american-life': {
+        'apiId': 'c98e2fa600b24af2a5806ee3b8ce5a47',
+        'title': 'This American Life',
+        'image': 'https://production.listennotes.com/podcasts/this-american-life-this-american-life-8190oLwUTlR-0iKakprjsG-.300x300.jpg',
+        'description': '<p>Each week we choose a theme. Then anything can happen. This American Life is true stories that unfold like little movies for radio. Personal stories with funny moments, big feelings, and surprising plot twists. Newsy stories that try to capture what it’s like to be alive right now. It’s the most popular weekly podcast in the world, and winner of the first ever Pulitzer Prize for a radio show or podcast. Hosted by Ira Glass and produced in collaboration with WBEZ Chicago.</p>',
+        'credits': {
+            'host': 'Ira Glass',
+            'publisher': 'WBEZ Chicago',
+            'network': 'PRX',
+        },
+    },
+};
+
+export default seriesData;
