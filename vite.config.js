f0d7ebb699e13c11006fa2301285c6fc0d6d251c
David Radick <dradick@radickalcreations.com>
2025-01-15 16:55:57 -0600

    Adding new global values

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const nodeEnv = process.env.NODE_ENV === "production" ? 'https://api.radickalcreations.com' : '/api';
+const API_PATH = process.env.NODE_ENV === "production" ? 'https://api.radickalcreations.com' : '/api';
+const PUBLIC_PATH = process.env.NODE_ENV === "production" ? '/demo/podcast-web-app' : '';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -20,6 +22,8 @@ export default defineConfig({
     }
   },
   define: {
-    BASE_API: JSON.stringify(nodeEnv),
+    BASE_API: JSON.stringify(API_PATH),
+    IMAGE_PATH: JSON.stringify(PUBLIC_PATH+"/images"),
+    ROOT_PATH: JSON.stringify("/demo/podcast-web-app")
   }
 });
