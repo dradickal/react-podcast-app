b4738d007726b0ae18c48f3d93354e9ea3a1cb70
David Radick <dradick@radickalcreations.com>
2025-01-15 16:57:57 -0600

    Adding Loading component

---
+++ src/Loading.jsx
@@ -0,0 +1,28 @@
+import './Loading.css';
+/** 
+ * Implemented Loading animation from codepen
+ * https://codepen.io/MyXoToD/pen/nqjjNN
+ */
+function Loading() {
+    return (
+        <div class="loader">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    )
+};
+
+export default Loading;
